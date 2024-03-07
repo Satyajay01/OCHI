40647858d94a2d4519c341c18b1f42ddef69778b
satyajay01 <satyajaydibya0@gmail.com>
2024-03-07 10:30:46 +0530

    Marquee complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import LandingPage from './Components/LandingPage'
+import Marquee from './Components/Marquee'
 
 function App() {
   return (
-    <div className='w-full h-screen text-white'>
+    <div className='w-full min-h-screen text-white bg-[#0f172a]'>
     <Navbar/>
     <LandingPage/>
+    <Marquee/>
     
     </div>
   )
---
+++ src/Components/Marquee.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Marquee() {
+  return (
+    <div className='w-fu py-10 bg-[#004d43] rounded-3xl pt-[18vh]'>
+        <div className='text border-t-2 border-b-2 border-zinc-500 flex gap-10 overflow-hidden whitespace-nowrap '>
+            <h1 className='text-[17.5vw] tracking-tighter leading-none font-semibold -mt-[5vh] '>WE ARE OCHI </h1>
+            <h1 className='text-[17.5vw] tracking-tighter leading-none font-semibold -mt-[5vh] '>WE ARE OCHI </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Marquee
